7fc64a6afcb2563c4e8275577aaa2f147b7d440d
Amandeep Singh <amansingh@Amandeeps-MacBook-Pro.local>
2018-11-01 22:06:18 -0700

    First Commit

---
+++ doonwebsite/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+# from django.contrib.auth.models import User
+#
+# # Create your models here.
+# class Inventory(models.Model):
+# title = models.CharField(max_length=255)
+# title = models.CharField(max_length=255)
